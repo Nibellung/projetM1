db9c2305a8fedd8c30c9c24c5a5c3279b80c4de8
UnkneesDwark <57356970+UnkneesDwark@users.noreply.github.com>
2021-05-18 16:00:52 +0200

    Update uplift.py

--- uplift.py
+++ uplift.py
@@ -7,6 +7,10 @@ Created on Wed Feb 10 13:15:19 2021
 #maj 27/03/21
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
 
 class Uplift(object):
     
@@ -39,3 +43,75 @@ class Uplift(object):
     
     def score(self,X,y):
         return np.sum(y == self.predict(X)) / len(X)
+    
+"""
+Entrée  : None
+Sortie  : Récupère les lecteurs dans le fichier crée bid.csv et convertit le fichier en matrice numpy
+"""
+
+def recupererLecteurFromBid():
+  data=pd.read_csv('./bid.csv')
+  data=data.to_numpy()
+  return data
+
+"""
+Entrée  : un entier numarticle
+Sortie  : Récupère les labels de chaque reviewers pour un article numarticle
+"""
+def recupererLabelforAnArticle(numarticle):
+  label=pd.read_csv('./score/score'+str(numarticle)+'.csv')
+  label=label.to_numpy()
+  return label
+
+"""
+Entrée  : les données récupérees grace a recupererLecteurFromBid
+Sortie  : les données séparées entre les données de traitement et les données de controle,
+ ainsi qu'une liste des identifiants liés séparés de la même facon
+"""
+def split_data(data):
+  data_control = []
+  id_control =[]
+  data_treatment = []
+  id_treatment = []
+  for i in data:
+    if i[1]==0: 
+      data_treatment.append(i)
+      id_treatment.append(i[0])
+    else :
+      data_control.append(i)
+      id_control.append(i[0])
+  data_control=sorted(data_control, key = lambda data : data[0])
+  data_control=np.array(data_control)
+  data_control=np.delete(data_control,0,1)
+  data_control=np.delete(data_control,0,1)
+  data_treatment=sorted(data_treatment, key = lambda data : data[0])
+  data_treatment=np.array(data_treatment)
+  data_treatment=np.delete(data_treatment,0,1)
+  data_treatment=np.delete(data_treatment,0,1)
+  id_control=np.array(id_control)
+  id_treatment=np.array(id_treatment)
+  return data_control,data_treatment, id_control,id_treatment
+
+"""
+Entrée  : les labels d'un article avec les reviwers associé, 
+          la liste des identifiants des reviwers du jeu de controle
+          et la liste des identifients des reviewers du jeu de traitement
+Sortie  : liste label_contol et label_traitement qui correnspondent au label respectivement
+          liés au jeu de controle et au jeu de traitement
+Note    : La fonction s'assure que chaque label soit bien associé au bon revierwer
+"""
+def association_label(label,id_control,id_treatment):
+  label_control=[]
+  label_treatment=[]
+  for i in label :
+    if np.any(id_control == i[0]) :
+      label_control.append(i)
+    else:
+      label_treatment.append(i)
+  label_control=sorted(label_control, key = lambda label : label[0])
+  label_control=np.array(label_control)
+  label_control=np.delete(label_control,0,1)
+  label_treatment=sorted(label_treatment, key = lambda label : label[0])
+  label_treatment=np.array(label_treatment)
+  label_treatment=np.delete(label_treatment,0,1)
+  return label_control,label_treatment
